2df9b9d95ebc5fc550b8967c63cd495b5bd0614c
nigel1513 <123634868+nigel1513@users.noreply.github.com>
2023-01-26 23:52:50 +0900

    Delete pages directory

--- pages/Contact/ABOUTME.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import json
-import requests
-from streamlit_lottie import st_lottie
-
-st.set_page_config(page_title="About Me!", page_icon="❓")
-st.title("About Me")
-st.subheader("")
