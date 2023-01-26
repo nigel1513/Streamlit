b95212e6cef19ed471440fe4b36697c54273d64c
nigel1513 <123634868+nigel1513@users.noreply.github.com>
2023-01-26 23:00:33 +0900

    Add files via upload

---
+++ pages/Contact/ABOUTME.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import json
+import requests
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title="About Me!", page_icon="❓")
+st.title("About Me")
+st.subheader("")
